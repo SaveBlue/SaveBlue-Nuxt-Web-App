a8eb6ef442bbc30244b2aa8f544eeb30640f9d15
sovintz <lan.sovinc@gmail.com>
2022-09-20 22:35:39 +0200

    Tried another nuxt.config.js fix.

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -69,7 +69,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : 'http://localhost:5000/api',
+    baseURL: process.env.NODE_ENV === 'dev' ? 'http://localhost:5000/api' : process.env.BASE_URL,
   },
 
   auth: {
